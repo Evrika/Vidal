60b9325ead9c0ffea4ba6632cc9d462c2ecc6642
7binary <7binary@gmail.com>
2015-06-11 17:06:25 +0300

    merge @ origin/mobile_site

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -32,7 +32,6 @@ class AppKernel extends Kernel
             new Gregwar\CaptchaBundle\GregwarCaptchaBundle(),
 			new Ornicar\ApcBundle\OrnicarApcBundle(),
 			new Liuggio\ExcelBundle\LiuggioExcelBundle(),
-			new Liip\ThemeBundle\LiipThemeBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
