90fb6e8c6ab16765d0e3e9fe68bcb4d1cc133230
Andrey Sokolov <walfireru@gmail.com>
2015-09-22 12:19:20 +0300

    Neirodoz

---
+++ src/Vidal/MainBundle/Controller/NeirodozController.php
@@ -0,0 +1,24 @@
+<?php
+namespace Vidal\MainBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Vidal\MainBundle\Entity\AstrazenecaFaq;
+
+class NeirodozController extends Controller
+{
+    /**
+     * @Route("/neiro_doz", name="shkola_gastrita")
+     * @Template("VidalMainBundle:NeiroDoz:home.html.twig")
+     */
+    public function numb11erAction()
+    {
+        $params = [];
+        return $params;
+    }
+}
